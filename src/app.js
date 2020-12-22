939f7cce3de78dacd8c21e841f02aafbffdb4cc2
Navaneeth <navaneethks1995@gmail.com>
2020-12-23 02:01:42 +0530

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const forecast = require("./utils/forecast");
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 const publicDirectoryPath = path.join(__dirname, "..", "public");
 const viewsPath = path.join(__dirname, "..", "templates", "views");
 const partialsPath = path.join(__dirname, "..", "templates", "partials");
@@ -78,6 +80,6 @@ app.get("*", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log(`Server started`);
+app.listen(PORT, () => {
+  console.log(`Server started on ${PORT}`);
 });
